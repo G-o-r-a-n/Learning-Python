6f613924c48d24abf73b8e7f87ed09538b2bdbf4
Goran Hourami <[github@hourami.co]>
2023-06-30 20:30:59 +0100

    Rename directories.

--- Task 2 - The String Data Type/strip_method.py
+++
@@ -1,6 +0,0 @@
-# Define 'hero' variable.
-hero = "$$$Superman$$$"
-
-# Display the variable 'hero' using strip() function to remove the unwanted
-# leading and trailing characters.
-print(hero.strip('$'))
