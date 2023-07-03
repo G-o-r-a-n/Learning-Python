d1da622498296c9a2c7cd2eb027865afe0fbbd2a
Goran Hourami <[github@hourami.co]>
2023-07-03 18:35:18 +0100

    Changed print statements.
    Changed the numerous single-line string print statements to multi-line strings for improved readability.

--- Task 05 - Beginner Control Structures - While Loop/while_loop.py
+++ Task 05 - Beginner Control Structures - While Loop/while_loop.py
@@ -1,14 +1,15 @@
 # Display welcome statement along with instructions.
-print("---------- Mean Average Calculator ----------")
 print(
-    "\n"
-    "This program will calculate the average of a series of numbers you enter."
-    "\n\n"
-    "Enter '-1' when you are ready to calculate the average."
-    "\n\n"
-    "----------------------------------------"
-    "\n\n"
-    )
+"""
+---------- Mean Average Calculator ----------
+    
+This program will calculate the average of a series of numbers you enter.
+    
+"Enter '-1' when you are ready to calculate the average."
+    
+----------------------------------------
+"""
+)
 
 # Create a list to store the user's numbers..
 list_of_numbers = []
@@ -30,12 +31,18 @@ while True:
         print("\nInvalid entry.\n")
 
 # Present the data gathered and calculated mean average to the user.
-print("----------------------------------------")
-print(f"Numbers entered: {list_of_numbers}\n")
+print(
+f"""
+----------------------------------------
 
-print(f"Number of entries: {len(list_of_numbers)}\n")
-
-print(f"Total sum: {sum(list_of_numbers)}\n")
-
-print(f"Average: {(sum(list_of_numbers) / len(list_of_numbers)):.2f}")
-print("----------------------------------------")
+Numbers entered: {list_of_numbers}
+    
+Number of entries: {len(list_of_numbers)}
+    
+Total sum: {sum(list_of_numbers)}
+    
+Average: {(sum(list_of_numbers) / len(list_of_numbers)):.2f}
+    
+----------------------------------------
+"""
+)
